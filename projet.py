cb362d8a5eab1e32b7c71c23d6457576d46990a4
ybakkali <yahyabakkali@hotmail.com>
2019-11-25 12:05:12 +0100

    modifs

--- projet.py
+++ projet.py
@@ -6,90 +6,86 @@ import csv
 import os
 from os import listdir
 from os.path import isfile, join
-from subprocess import check_output
 import matplotlib.pyplot as plt
 
 
 def show_rgb_img(img):
     """Convenience function to display a typical color image"""
-    return plt.imshow(cv2.cvtColor(img, cv2.CV_32S))
+    return plt.imshow(cv2.cvtColor(cv2.imread(img), cv2.CV_32S))
 
+def show_gray_img(img):
+    """Convenience function to display a typical gray image"""
+    return plt.imshow(cv2.cvtColor(cv2.imread(img), cmap='gray'))
 
-#plt.imshow(octo_front_gray, cmap='gray');
+def to_gray(img_path):
+    return cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2GRAY)
 
+def gen_sift_features(gray_img):
+
+    sift = cv2.xfeatures2d.SIFT_create()
+    # kp is the keypoints
+    # desc is the SIFT descriptors, they're 128-dimensional vectors
+    kp, desc = sift.detectAndCompute(gray_img, None)
+    return kp, desc
 
 def show_sift_features(gray_img, color_img, kp):
     return plt.imshow(cv2.drawKeypoints(gray_img, kp, color_img.copy()))
 
 #------------------------------------------------------------------------------
 
-def to_gray(img_path):
-    return cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2GRAY)
-
-def gen_sift_features(gray_img):
-    sift = cv2.xfeatures2d.SIFT_create()
-
-    # kp is the keypoints
-    # desc is the SIFT descriptors, they're 128-dimensional vectors
-    # that we can use for our final features
-    kp, desc = sift.detectAndCompute(gray_img, None)
-    return kp, desc
 
 #Loading data
 
-print(check_output(["ls", "waste-classification-data"]).decode("utf8"))
+#print(check_output(["ls", "waste-classification-data"]).decode("utf8"))
 
 # Any results you write to the current directory are saved as output.
-mydirvalues = [d for d in os.listdir(os.path.dirname(os.path.abspath(__file__)))]
+#mydirvalues = [d for d in os.listdir(os.path.dirname(os.path.abspath(__file__)))]
 #print(mydirvalues)
-onlyfiles = [f for f in listdir("waste-classification-data/DATASET/TRAIN/") if isfile(join("waste-classification-data/DATASET/TRAIN/", f))]
-#print(onlyfiles)
 
-dir_names = [d for d in listdir("waste-classification-data/DATASET/TRAIN/") if not isfile(join("waste-classification-data/DATASET/TRAIN/", d))]
-#print(dir_names)
+train_dataset = "waste-classification-data/DATASET/TRAIN/"
+test_dataset =  "waste-classification-data/DATASET/TEST/"
 
-file_paths = {}
+train_files = [f for f in listdir(train_dataset) if isfile(join(train_dataset, f))]
+test_files = [f for f in listdir(test_dataset) if isfile(join(test_dataset, f))]
+
+train_dir_names = [d for d in listdir(train_dataset) if not isfile(join(train_dataset, d))]
+test_dir_names = [d for d in listdir(test_dataset) if not isfile(join(test_dataset, d))]
+
+train_file_paths = {}
+test_file_paths = {}
+
 class_num = 0
-for d in dir_names:
-     fnames = [f for f in listdir("waste-classification-data/DATASET/TRAIN/"+d+"/") if isfile(join("waste-classification-data/DATASET/TRAIN/"+d+"/", f))]
-     #print(fnames)
-     file_paths[(d, class_num, "waste-classification-data/DATASET/TRAIN/"+d+"/")] = fnames
+for d in train_dir_names:
+     train_fnames = [f for f in listdir(train_dataset+d+"/") if isfile(join(train_dataset+d+"/", f))]
+     train_file_paths[(d, class_num, train_dataset+d+"/")] = train_fnames
      class_num += 1
 
-#Feature Extraction
+for d in test_dir_names:
+     test_fnames = [join(test_dataset+d+"/", f) for f in listdir(test_dataset+d+"/") if isfile(join(test_dataset+d+"/", f))]
 
-# General steps:
-# Extract feature from each file as SIFT.
-# map each to feature space... and train some kind of classifier on that. SVM is a good choice.
-# do the same for each feature in test set...
+#Feature Extraction
 
 training_data = np.array([])
 training_labels = np.array([])
 
-for key in file_paths:
-    print(key)
-    category = key[1]
-    directory_path = key[2]
-    file_list = file_paths[key]
-    # shuffle this list, so we get random examples
-    np.random.shuffle(file_list)
-
-    # Stop early, while testing, so it doesn't take FOR-EV-ER (FOR-EV-ER)
-    i = 0
+for key in train_file_paths:
+    category, directory_path = key[1], key[2]
+    file_list = train_file_paths[key]
 
     # read in the file and get its SIFT features
-    for fname in file_list:
+    nb = 300 #len(file_list)
+    for fname in file_list[:nb]:
+        np.random.shuffle(file_list)
         fpath = directory_path + fname
-        print(fpath)
-        print("Category = " + str(category))
-        # extract features!
-        gray = to_gray(fpath)
-        gray = cv2.resize(gray, (400, 250))  # resize so we're always comparing same-sized images
-                                             # Could also make images larger/smaller
-                                             # to tune for greater accuracy / more speedd
+        #print(fpath)
+        #print("Category = " + str(category))
 
-        detector = cv2.xfeatures2d.SIFT_create()
-        kp, des = detector.detectAndCompute(gray, None)
+        # extract features
+        gray = to_gray(fpath)
+        gray = cv2.resize(gray, (400, 250))
+        # resize so we're always comparing same-sized images, could also make images larger/smaller to tune for greater accuracy / more speedd
+
+        kp, des = gen_sift_features(gray)
 
         # This is to make sure we have at least 100 keypoints to analyze
         # could also duplicate a few features if needed to hit a higher value
@@ -113,80 +109,47 @@ for key in file_paths:
 
         training_labels = np.append(training_labels,category)
 
-        # early stop
-        i += 1
-        if i > 50:
-            break
-
-#Fit the SVM
-
-# Alright! Now we've got features extracted and labels
-X = training_data
-y = training_labels
-y = y.reshape(y.shape[0],)
-
 # Create and fit the SVM
-# Fitting should take a few minutes
 clf = svm.SVC(kernel='linear', C = 1.0, probability=True)
-clf.fit(X,y)
-
-#Make a Prediction
-
-# Now, extract one of the images and predict it
-gray = to_gray('waste-classification-data/DATASET/TEST/O/O_12583.jpg')
-kp, des = detector.detectAndCompute(gray, None)
-#show_sift_features(gray,cv2.imread(('waste-classification-data/DATASET/TEST/O/O_12583.jpg')),kp)
-des = des[0:100, :]   # trim vector so all are same size
-vector_data = des.reshape(1, 12800)
-
-print("Linear SVM Prediction:")
-print(clf.predict(vector_data))        # prints highest probability class, only
-print(clf.predict_proba(vector_data))  # shows all probabilities for each class.
-                                       #    need this for the competition
-
-
-#Save the SVM for later Use
-
-# save SVM model
-# joblib.dump(clf, 'filename.pkl')
-# to load SVM model, use:  clf = joblib.load('filename.pkl')
+clf.fit(training_data,training_labels.reshape(training_labels.shape[0],))
 
 #Predict the whole Data Set
 
-# early stoppage...
-# only do 10
-"""
-i = 0
-for f in fnames:
-    file_name = "waste-classification-data/DATASET/TEST/" + f
+nb = 100 #len(test_fnames)
+for file_name in test_fnames[:nb]:
+    np.random.shuffle(test_fnames)
     print("---Evaluating File at: " + file_name)
-    gray = cv2.imread(file_name, 0)  # Correct is LAG --> Class 3
+    gray = to_gray(file_name)
     gray = cv2.resize(gray, (400, 250))  # resize so we're always comparing same-sized images
-    kp1, des1 = detector.detectAndCompute(gray, None)
+    kp, des = gen_sift_features(gray)
 
     # ensure we have at least 100 keypoints to analyze
-    if len(kp1) < 100:
+    if len(kp) < 100:
         # and duplicate some points if necessary
-        current_len = len(kp1)
+        current_len = len(kp)
         vectors_needed = 100 - current_len
-        repeated_vectors = des1[0:vectors_needed, :]
-        # concatenate repeats onto des1
-        while len(des1) < 100:
-            des1 = np.concatenate((des1, repeated_vectors), axis=0)
+        repeated_vectors = des[0:vectors_needed, :]
+        # concatenate repeats onto des
+        while len(des) < 100:
+            des = np.concatenate((des, repeated_vectors), axis=0)
         # duplicate data just so we can run the model.
-        des1[current_len:100, :] = des1[0:vectors_needed, :]
+        des[current_len:100, :] = des[0:vectors_needed, :]
 
-    np.random.shuffle(des1)  # shuffle the vector so we get a representative sample
-    des1 = des1[0:100, :]   # trim vector so all are same size
-    vector_data = des1.reshape(1, 12800)
+    np.random.shuffle(des)  # shuffle the vector so we get a representative sample
+    des = des[0:100, :]   # trim vector so all are same size
+    vector_data = des.reshape(1, 12800)
+
+    #show_rgb_img(file_name)
+    #plt.show()
     print("Linear SVM Prediction:")
-    print(clf.predict(vector_data))
-    svm_prediction = clf.predict_proba(vector_data)
+    print(clf.predict(vector_data)) # prints highest probability class, only
+    svm_prediction = clf.predict_proba(vector_data) # shows all probabilities for each class.
     print(svm_prediction)
 
+    """
     # format list for csv output
     csv_output_list = []
-    csv_output_list.append(f)
+    csv_output_list.append(file_name)
     for elem in svm_prediction:
         for value in elem:
             csv_output_list.append(value)
@@ -197,9 +160,10 @@ for f in fnames:
 
     # and append this file to the output_list (of lists)
     prediction_output_list.append(csv_output_list)
+    """
 
-    # Uncomment to stop early
-    if i > 10:
-        break
-    i += 1
-"""
+#Save the SVM for later Use
+
+# save SVM model
+# joblib.dump(clf, 'filename.pkl')
+# to load SVM model, use:  clf = joblib.load('filename.pkl')
